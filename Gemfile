273f8d1e64c278fa7a84da1e385a2832e1ec8c45
Anna <annamtoman@gmail.com>
2023-03-14 15:34:02 -0500

    updates

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem "pg", "~> 1.1"
 gem "puma", "~> 5.0"
 
 gem 'nokogiri'
+gem 'pry', '~> 0.14.2'
 
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -3,5 +3,18 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-    
+    def scrape_city_urls
+        illinois_url = 'http://museumsusa.org/museums/?k=1271400%2cState%3aIL'
+
+        html = URI.open(illinois_url)
+
+        doc = Nokogiri::HTML(html)
+
+        cities = doc.css('#ctl08_ctl00_rptChildNodes_dlItems_1').css('.ui small text')
+
+        binding.pry
+    end
 end
+
+scrape = Scraper.new
+scrape.scrape_city_urls
